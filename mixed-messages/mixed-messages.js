790e5cf837b7c3a08f965ea319f68c168764f478
Tylor Mondragon <twaitsmo@alumni.nd.edu>
2021-11-08 16:01:14 -0600

    Added functionality to a phrase object to generate random messages when a user runs the program

--- mixed-messages/mixed-messages.js
+++ mixed-messages/mixed-messages.js
@@ -1,5 +1,14 @@
 const phrase = {
-    start: [],
-    action: [],
-    end: []
+    start: ['You will travel the world!', 'You will get a job and stay living at home.', 'You will marry and have ten kids.'],
+    middle: ['You will have 16 kids.', 'You will have 8 kids.', 'You will have no kids and 3 dogs.', 'You will have a dog and cat.', 'You will have a cat.'],
+    end: ['You will die at 88.', 'You will retire and live until 102.', 'You will live forever!']
 }
+
+function buildRandomMessage() {
+    let start = phrase.start[Math.floor(Math.random() * phrase.start.length)];
+    let middle = phrase.middle[Math.floor(Math.random() * phrase.middle.length)];
+    let end = phrase.end[Math.floor(Math.random() * phrase.end.length)];
+    console.log(start + '\n' + middle + '\n' + end);
+}
+
+buildRandomMessage();
